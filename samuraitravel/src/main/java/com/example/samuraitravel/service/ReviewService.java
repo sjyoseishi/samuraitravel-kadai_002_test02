546f9846f9dde1d25dc59913a619a2feb4394bec
sjyoseishi <yiting19841104@gmail.com>
2024-08-26 23:35:18 +0900

    kadai_002をプッシュ

---
+++ samuraitravel/src/main/java/com/example/samuraitravel/service/ReviewService.java
@@ -0,0 +1,61 @@
+package com.example.samuraitravel.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.repository.ReviewRepository;
+import com.example.samuraitravel.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.samuraitravel.form.EditForm;
+import com.example.samuraitravel.entity.Review;
+import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.RegisterForm;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private final ReviewRepository reviewRepository;
+	private final HouseRepository houseRepository;
+	private final UserRepository userRepository;
+
+
+
+    public List<Review> getReviewsByHouseId(Integer houseId) {
+        return reviewRepository.findByHouseIdOrderByCreatedAtDesc(houseId);
+    }
+
+
+
+	public ReviewService(ReviewRepository reviewRepository, HouseRepository houseRepository,
+			UserRepository userRepository) {
+		this.reviewRepository = reviewRepository;
+		this.houseRepository = houseRepository;
+		this.userRepository = userRepository;
+	}
+
+	//レビューの更新
+	@Transactional
+	public void update(EditForm EditForm) {
+		Review review = reviewRepository.getReferenceById(EditForm.getId());
+		review.setStar(EditForm.getStar());
+		review.setReview(EditForm.getReview());
+		reviewRepository.save(review);
+	}
+
+	//新規レビューをDBに保存
+	@Transactional
+	public void create(RegisterForm RegisterForm) {
+		Review review = new Review();
+		House house = houseRepository.getReferenceById(RegisterForm.getHouseId());
+		User user = userRepository.getReferenceById(RegisterForm.getUserId());
+		review.setHouse(house);
+		review.setUser(user);
+		review.setStar(RegisterForm.getStar());
+		review.setReview(RegisterForm.getReview());
+		reviewRepository.save(review);
+	}
+}
